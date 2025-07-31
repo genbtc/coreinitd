100e3793b8769b2ceae02c5ae2deba2115487b31
genBTC <genBTC@gmx.com>
2025-07-31 14:40:08 -0400

    src/coreinitd/service_manager.h: Define service state + metadata

---
+++ src/coreinitd/service_manager.h
@@ -0,0 +1,23 @@
+#ifndef COREINITD_SERVICE_MANAGER_H
+#define COREINITD_SERVICE_MANAGER_H
+
+#include "unit_loader.h"
+
+typedef enum {
+    SERVICE_INACTIVE,
+    SERVICE_STARTING,
+    SERVICE_ACTIVE,
+    SERVICE_FAILED
+} ServiceState;
+
+typedef struct {
+    Unit *unit;
+    pid_t pid;
+    ServiceState state;
+} ServiceEntry;
+
+int service_manager_start(Unit *unit);
+void service_manager_reap(pid_t pid);
+void service_manager_status(void);
+
+#endif
