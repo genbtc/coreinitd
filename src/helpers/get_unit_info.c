4325bc827513618ed7c082e39dfcce0d55a50f36
genBTC <genBTC@gmx.com>
2025-07-31 13:40:09 -0400

    get_unit_info.c: Future diagnostic helper to extract fields from a .service or .socket file. For now, it just dumps the file.

--- src/helpers/get_unit_info.c
+++ src/helpers/get_unit_info.c
@@ -1,1 +1,25 @@
-/* Stub file: get_unit_info.c */
+// get_unit_info.c
+// Simple dump of unit file content
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s unit-file\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    if (!f) {
+        perror("fopen");
+        return 1;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), f)) {
+        fputs(line, stdout);
+    }
+
+    fclose(f);
+    return 0;
+}
