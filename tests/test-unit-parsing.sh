0e2c7e4934d57049c5c02d468c1995055d569445
genBTC <genBTC@gmx.com>
2026-05-05 22:47:49 -0400

    systemd-user/: add assortment of user services so we can test and parse real data

--- tests/test-unit-parsing.sh
+++ tests/test-unit-parsing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Stub test script
-gcc -Isrc -o test-loader tests/test-unit-parsing.c src/coreinitd/unit_loader.c
-./test-loader
+gcc -Isrc -o build/test-loader tests/test-unit-parsing.c src/coreinitd/unit_loader.c
+build/test-loader
+#Loaded example service, UNIX socket, and timer units successfully
